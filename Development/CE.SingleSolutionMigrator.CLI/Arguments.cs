78fb94c2704c6f2e1a0ee765f26f80fb43886807
Kris Culin <Kris.Culin@bentley.com>
2023-07-12 12:46:14 -0400

    Just the start of the SingleSolutionMigrator rewrite

---
+++ Development/CE.SingleSolutionMigrator.CLI/Arguments.cs
@@ -0,0 +1,13 @@
+﻿// Arguments.cs
+// Copyright (c) Kris Culin. All Rights Reserved.
+
+using CommandLine;
+
+namespace CE.SingleSolutionMigrator.CLI
+{
+    internal class Arguments
+    {
+        [Option('r', longName: "RootPath", Required = true, HelpText = "The full path to the source code being migrated.")]
+        public required string RootPath { get; set; }
+    }
+}
---
+++ Development/CE.SingleSolutionMigrator.CLI/Program.cs
@@ -0,0 +1,8 @@
+﻿using CE.SingleSolutionMigrator.CLI;
+using CommandLine;
+
+Parser.Default.ParseArguments<Arguments>(args)
+    .WithParsed<Arguments>(o =>
+    {
+        Console.WriteLine(o.RootPath);
+    });
