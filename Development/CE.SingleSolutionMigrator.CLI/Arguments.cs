ea73f2636f72b12a75b07af1ca8f364e01c4aec7
Kris Culin <Kris.Culin@bentley.com>
2023-07-12 13:18:29 -0400

    options

--- Development/CE.SingleSolutionMigrator.CLI/Arguments.cs
+++ Development/CE.SingleSolutionMigrator.CLI/Arguments.cs
@@ -9,5 +9,18 @@ namespace CE.SingleSolutionMigrator.CLI
     {
         [Option('r', longName: "RootPath", Required = true, HelpText = "The full path to the source code being migrated.")]
         public required string RootPath { get; set; }
+        [Option('s', longName: "Solution", Required = true, HelpText = "The full path and filename of the solution to migrate.")]
+        public required string Solution { get; set; }
+
+        [Option('p', longName: "Usep4", Group = "Perforce", Default = false, HelpText = "True to use p4 to check out files.")]
+        public bool UseP4 { get; set; }
+        [Option('v', longName: "Server", Group = "Perforce", Default = "", HelpText = "The perforce server")]
+        public string ServerName { get; set; } = string.Empty;
+        [Option('u', longName: "UserName", Group = "Perforce", Default = "", HelpText = "The user name for logging into p4.")]
+        public string UserName { get; set; } = string.Empty;
+        [Option('w', longName: "Password", Group = "Perforce", Default = "", HelpText = "The password to use to log into p4.")]
+        public string Password { get; set; } = string.Empty;
+        [Option('w', longName: "Workspace", Group = "Perforce", Default = "", HelpText = "The workspace to use for checkout.")]
+        public string Workspace { get; set; } = string.Empty;
     }
 }
