f1be9390d5eae3ae43b6065b0770732f64ac4c3c
Kris Culin <Kris.Culin@bentley.com>
2023-07-17 05:34:32 -0400

    Added tool to set/remove the read-only state of files under a given path. Dupes some debug profiles to adjust paths (differ from home)

---
+++ Development/CE.SingleSolutionMigrator/Tools/FileAttributeTool.cs
@@ -0,0 +1,45 @@
+﻿// FileAttributeTool.cs
+// Copyright (c) 2023 Kris Culin. All Rights Reserved.
+
+using ShellProgressBar;
+
+namespace CE.SingleSolutionMigrator.Tools
+{
+    public class FileAttributeTool
+    {
+        #region Constructor
+        public FileAttributeTool()
+        {
+
+        }
+        #endregion
+
+        #region Public Methods
+        public void Execute(string rootPath, bool isReadOnly)
+        {
+            var files = Directory.GetFiles(rootPath, "*", SearchOption.AllDirectories);
+            using (var pbar = new ProgressBar(files.Length, "Applying file attribute...",
+                new ProgressBarOptions
+                {
+                    ForegroundColor = ConsoleColor.Red,
+                    BackgroundColor = ConsoleColor.White,
+                    ForegroundColorDone = ConsoleColor.Blue,
+                    DisplayTimeInRealTime = true,
+                }))
+            {
+                foreach (var file in files)
+                {
+                    pbar.Tick(file);
+
+                    if (isReadOnly)
+                        File.SetAttributes(file, File.GetAttributes(file) & FileAttributes.ReadOnly);
+                    else
+                        File.SetAttributes(file, File.GetAttributes(file) ^ FileAttributes.ReadOnly);
+                }
+
+                pbar.Message = "Process completed.";
+            }
+        }
+        #endregion
+    }
+}
